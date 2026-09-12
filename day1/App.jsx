7d4ccd873735a55615430b6669c1df3319db634b
Nisha Jadhav <nishajadhav032005@gmail.com>
2026-09-12 15:41:06 +0530

    Add files via upload

---
+++ day1/App.jsx
@@ -0,0 +1,24 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <div className="profile-card">
+        <h1>My React Profile</h1>
+
+        <h2>Nisha Jadhav</h2>
+
+        <p><strong>Course:</strong> BCA</p>
+
+        <p>
+          I am learning Python Full Stack Development and
+          improving my programming skills.
+        </p>
+
+        <button>View Profile</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
